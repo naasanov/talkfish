5220da4385125674f7c36c03c426ac34ac2ae71f
manyuagashe <manyu@unc.edu>
2025-03-29 12:57:53 -0400

    commit message for background.js

---
+++ extension/src/background.js
@@ -0,0 +1,14 @@
+chrome.runtime.onInstalled.addListener(() => {
+    console.log("Mock Interview Feedback Extension Installed");
+  });
+  
+  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === "startRecording") {
+      // Handle recording initiation
+      sendResponse({ status: "Recording started" });
+    } else if (message.action === "stopRecording") {
+      // Handle stopping recording
+      sendResponse({ status: "Recording stopped" });
+    }
+  });
+  
